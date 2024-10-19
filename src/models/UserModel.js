6c0690177dab3753e47dde867d86651ecb0f6e9c
FirstOne2308 <phamquangvu2308@gmail.com>
2024-10-19 22:00:07 +0700

    update models

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,18 +5,17 @@ const userSchema = new mongoose.Schema(
     user_name: { type: String, required: true }, // Customer name
     user_password: { type: String, required: true }, // Password
     user_avt_img: { type: String }, // Avatar image
-    user_email: { type: String, required: true, unique: true }, // Email
-    user_phone: { type: String, required: true, unique: true }, // Phone number
+    user_email: { type: String, unique: true }, // Email
+    user_phone: { type: String, unique: true }, // Phone number
     user_address: { 
-        type: {
-            province: { type: String, required: true }, // Province
-            district: { type: String, required: true }, // District
-            commune: { type: String, required: true } // Commune
-        }, 
-        required: true 
+      home_number: {type: Number},
+      province: { type: String}, // Province
+      district: { type: String}, // District
+      commune: { type: String} // Commune
     }, // Address
     user_birth: { type: Date }, // Date of birth
-    user_sex: { type: String, enum: ['Male', 'Female', 'Other'] } // Gender
+    user_sex: { type: String, enum: ['Nam', 'Nữ', 'Khác'], default: "Nam" } ,// Gender
+    is_delete : {type: Boolean, default: false}
   },
   {
     timestamps: true
