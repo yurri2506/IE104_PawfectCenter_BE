7d15146f25e594b7e54195363cf136ca08cfbb57
yurri2506 <22520590@gm.uit.edu.vn>
2024-10-26 19:35:09 +0700

    update model product

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,26 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const variantSchema = new mongoose.Schema(
+  {
+    product_category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    pet_age: {type: Number, required: true},
+    product_color: { type: String },
+    product_weight: { type: String }, 
+    product_size: { type: String }, 
+    variant_img: { type: String }, 
+    product_price: { type: Number, required: true },
+    product_countInStock: { type: Number, required: true },
+  },
+  { _id: false }
+);
 
 const productSchema = new mongoose.Schema(
   {
-    product_type: { type: String, required: true },
-    product_model: { type: [String], required: true }, // Sử dụng mảng cho mẫu mã
-    product_name: { type: String, required: true },
-    product_img: { type: [String] }, // Mảng chứa nhiều hình ảnh
-    product_price: { type: Number, required: true },
+    product_title: { type: String, required: true },
+    product_img: { type: [String] }, 
     product_description: { type: String, required: true },
-    product_display: { type: Boolean, required: true, default: false},
-    product_famous: { type: Boolean, required: true, default: false}, // sản phẩm nôỉ bật
-    product_countInStock: { type: Number, required: true },
-    product_rate: { type: Number }, // Sẽ được tính toán từ Feedbacks
-    product_feedback: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' }], // Tham chiếu đến collection Feedback
-    product_selled: { type: Number, required: true },
-    product_percent_discount: { type: Number },
-    is_delete : {type: Boolean, default: false}
+    product_display: { type: Boolean, required: true, default: false },
+    product_famous: { type: Boolean, required: true, default: false }, 
+    product_rate: { type: Number },
+    product_feedback: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Feedback" },
+    ],
+    product_selled: { type: Number, required: true }, 
+    product_percent_discount: { type: Number }, 
+    is_delete: { type: Boolean, default: false }, 
+    variants: [variantSchema],
+    product_category: {}
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
