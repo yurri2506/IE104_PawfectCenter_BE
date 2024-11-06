ad881612de93e2af71f8a93a508332ae1537b59c
yurri2506 <22520590@gm.uit.edu.vn>
2024-11-06 19:36:41 +0700

    Update model Discount

--- src/models/Discount.model.js
+++ src/models/Discount.model.js
@@ -1,19 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const discountSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    discount_type: { type: String, enum: ['shipping', 'voucher'], required: true },
-    description: { type: String },
-    start_day: { type: Date },
-    end_day: { type: Date },
-    amount: { type: Number },
-    is_displayed: { type: Boolean, default: false }
+    discount_title: { type: String, required: true },
+    discount_type: {
+      type: String,
+      enum: ["shipping", "voucher"],
+      required: true,
+    },
+    discount_description: { type: String },
+    discount_start_day: { type: Date },
+    discount_end_day: { type: Date },
+    discount_amount: { type: Number },
+    is_displayed: { type: Boolean, default: false },
+    discount_number: { type: Number, required: true }, // phần trăm giảm giá
+    discount_condition: [
+      {
+        price_total_order: { type: Number, required: true },
+        category_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Category",
+          default: null,
+        },
+      },
+    ],
   },
   {
-    timestamps: true
+    timestamps: true,
+  },
+  {
+    collection: "Discount",
   }
 );
 
-const Discount = mongoose.model('Discount', discountSchema);
+const Discount = mongoose.model("Discount", discountSchema);
 module.exports = Discount;
