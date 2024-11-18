0425f4ef4ca7624a1f5abe520163578b17a29d07
FirstOne2308 <phamquangvu2308@gmail.com>
2024-11-18 21:47:21 +0700

    update Notifiction.model; add create, getAll, getDetail Notifiction

--- src/models/Notification.model.js
+++ src/models/Notification.model.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const notificationSchema = new mongoose.Schema(
   {
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    type: { type: String, enum: ['Tin nhắn', 'Khuyến mãi', 'Tình trạng đơn hàng', 'Tài khoản'], required: true },
-    content: { type: String, required: true },
+    order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order'},
+    product_id: { type: mongoose.Schema.Types.ObjectId, ref: '{Product'},
+    notify_type: { type: String, enum: ['Tin nhắn', 'Khuyến mãi', 'Tình trạng đơn hàng', 'Tài khoản', 'Sản phẩm'], required: true },
+    notify_title: { type: String, required: true },
+    notify_desc: { type: String, required: true },
     isRead: { type: Boolean, default: false }
   },
   {
-    timestamps: true
+    timestamps: true,
+    collection: 'Notification'
   }
 );
 
