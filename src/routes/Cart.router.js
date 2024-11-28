bb6ffe96239b1c695b628bf1ed524e00b5a2efaf
yurri2506 <22520590@gm.uit.edu.vn>
2024-11-28 23:43:30 +0700

    update Cart

--- src/routes/Cart.router.js
+++ src/routes/Cart.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router()
 const CartController = require('../controllers/Cart.controller');
+const { authUserMiddleWare } = require("../middleware/AuthMiddleWare");
 
 router.post('/create', CartController.createCart);
 router.patch('/update/:id', CartController.updateCart);
-router.get('/:userId/get-all-product', CartController.getAllProductByUserId);
+router.get('/get-all-product/:id', authUserMiddleWare, CartController.getAllProductByUserId);
 router.get('/cart/:userId/search', CartController.searchProductsInCart);
 
 module.exports = router
